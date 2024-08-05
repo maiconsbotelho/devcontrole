35af9fcb0246f98544fb7f23cda38a6d790435d9
Maicon Botelho <maicon.sbotelho@hotmail.com>
2024-08-05 17:57:15 -0300

    find: init

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import Header from '@/components/header';
+import { AuthProvider } from '@/providers/auth';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Dev Controle - Seu sistema de gerenciamento.',
+  description:
+    'Gerencie seus clientes e atendimentos de forma simples e eficiente.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider>
+          <Header />
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
